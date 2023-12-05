8bbf80f9e042cec7f637834d17432fb29c60626f
mlandukid <mlandu98@gmail.com>
2023-12-05 21:45:07 +0200

    Enhanced application with async requests and updated requirements.txt

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 from flask import Flask, jsonify
-import requests
-from concurrent.futures import ThreadPoolExecutor
+import asyncio
+import aiohttp
 
 app = Flask(__name__)
 
-def fetch_social_media_data(url):
+async def fetch_social_media_data(session, url):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        return len(data)
+        async with session.get(url) as response:
+            response.raise_for_status()
+            data = await response.json()
+            return len(data)
     except Exception:
-        return 0 
+        return 0  # Return 0 in case of any error
+
+async def get_social_media_activity():
+    social_media_urls = [
+        "https://takehome.io/twitter",
+        "https://takehome.io/facebook",
+        "https://takehome.io/instagram"
+    ]
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_social_media_data(session, url) for url in social_media_urls]
+        return await asyncio.gather(*tasks)
 
 @app.route("/social_network_activity")
 def social_network_activity():
-    social_media_urls = {
-        "twitter": "https://takehome.io/twitter",
-        "facebook": "https://takehome.io/facebook",
-        "instagram": "https://takehome.io/instagram"
-    }
-
-    with ThreadPoolExecutor() as executor:
-        results = list(executor.map(fetch_social_media_data, social_media_urls.values()))
-
-    json_response = dict(zip(social_media_urls.keys(), results))
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    results = loop.run_until_complete(get_social_media_activity())
+    social_media = ["twitter", "facebook", "instagram"]
+    json_response = dict(zip(social_media, results))
     return jsonify(json_response)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
